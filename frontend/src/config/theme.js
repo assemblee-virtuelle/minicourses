e0557169443a781bec3a0198071218ee0648ab9c
srosset81 <srosset81@gmail.com>
2022-06-22 16:01:52 +0200

    Update theme and logo

--- frontend/src/config/theme.js
+++ frontend/src/config/theme.js
@@ -1,7 +1,7 @@
 import { theme } from '@semapps/archipelago-layout';
 
-theme.palette.primary.main = '#bdc900';
-theme.palette.secondary.main = '#6b7e9b';
+theme.palette.primary.main = '#b9cd00';
+theme.palette.secondary.main = '#5a696b';
 theme.typography.h3.lineHeight = 1;
 theme.typography.h6.fontSize = 24;
 theme.typography.h6.lineHeight = 1.1;
